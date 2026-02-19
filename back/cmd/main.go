4bbf215af0fbb817b2f3b9cbcc8f31d9c1d94907
BulatYan <bulat5@outlook.com>
2026-02-19 22:01:43 +0300

    add html + update main

--- back/cmd/main.go
+++ back/cmd/main.go
@@ -2,22 +2,52 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 	"ped_poject/config"
 	"ped_poject/database"
 	"ped_poject/routes"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	log.Println("Testing config and database...")
 
-	// Просто загружаем конфиг и подключаемся к БД
+	// Загружаем конфиг и подключаемся к БД
 	dbConfig := config.LoadDB()
 	db := database.Connect(dbConfig)
-	database.ApplyMigrations(db, "internal/migrations")
+	database.ApplyMigrations(db, "back/migrations")
 	defer db.Close()
 
 	userRepo := database.NewUserRepository(db)
 	r := routes.SetupRouter(userRepo)
+
+	// Получаем абсолютный путь до файла
+	absPath, err := filepath.Abs("../front/register.html")
+	if err != nil {
+		log.Printf("Error getting absolute path: %v", err)
+	}
+
+	r.GET("/", func(c *gin.Context) {
+		log.Println("Request received for /")
+
+		// Проверяем существование файла
+		_, err := os.Stat(absPath)
+		if os.IsNotExist(err) {
+			log.Printf("File not found: %v", err)
+			c.JSON(500, gin.H{"error": "register.html not found"})
+			return
+		} else if err != nil {
+			log.Printf("Error checking file: %v", err)
+			c.JSON(500, gin.H{"error": "Failed to check register.html"})
+			return
+		}
+		log.Printf("Absolute path to register.html: %v", absPath)
+
+		// Отдаем файл
+		c.File(absPath)
+	})
 
 	log.Println("✅ Everything works!")
 	r.Run("0.0.0.0:8080")
--- back/database/migrate.go
+++ back/database/migrate.go
@@ -5,23 +5,36 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 )
 
 func ApplyMigrations(db *sql.DB, migrationsPath string) {
+	// Получаем список всех файлов с расширением .sql
 	files, err := filepath.Glob(filepath.Join(migrationsPath, "*.sql"))
 	if err != nil {
-		log.Fatalf("migrations file error: %v", err)
+		log.Fatalf("Error finding migration files: %v", err)
 	}
+
+	// Сортируем файлы по имени, чтобы применялись в правильном порядке
+	sort.Strings(files)
+
+	// Применяем каждую миграцию
 	for _, file := range files {
 		content, err := os.ReadFile(file)
 		if err != nil {
-			log.Fatalf("reading %v error: %v", file, err)
+			log.Printf("Error reading migration file %s: %v", file, err)
+			continue // продолжаем с остальными миграциями
 		}
+
+		// Выполняем SQL запрос из файла
 		_, err = db.Exec(string(content))
 		if err != nil {
-			log.Fatalf("applying %v error: %v", file, err)
+			log.Printf("Error applying migration %s: %v", file, err)
+			continue // продолжаем с остальными миграциями
 		}
-		log.Printf("migration %v succesfully applied\n", file)
+
+		log.Printf("Migration %s successfully applied", file)
 	}
-	log.Println("all migrations succesfully applied")
+
+	log.Println("All migrations processed")
 }
